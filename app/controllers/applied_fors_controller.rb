c6d6a5eb761129947ac6617b9da6890a57ae8271
Luffyv2.0 <devenderyadav1098@gmail.com>
2021-11-22 22:47:16 +0530

    resolved error of sign up

--- app/controllers/applied_fors_controller.rb
+++ app/controllers/applied_fors_controller.rb
@@ -1,17 +1,17 @@
 class AppliedForsController < ApplicationController
-    def new
+    def index
         @applied_for = AppliedFor.new
     end
     def create
-        @applied_for = AppliedFor.new(applied_for_params)
+        @applied_for = AppliedFor.create(applied_for_params)
         if @applied_for.save
-            redirect_to jseekerd_path, notice: "logged in successfully"
+            redirect_to jseeker_path(@jseeker), notice: "Applied successfully"
         else
             render :new
         end
     end
     private
     def applied_for_params
-      params.require(:applied_for).permit()
+      params.require(:applied_for).permit(:status, :job_id, :jseeker_id)
     end
 end
--- app/models/jseeker.rb
+++ app/models/jseeker.rb
@@ -1,5 +1,7 @@
 class Jseeker < ApplicationRecord
     has_secure_password
+    has_many :applied_fors
+    has_many :jobs, through: :applied_fors
 
     validates :email, presence: true, format: {with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: "must be valid email" }, uniqueness: {message: "email already used"}
     validates :mobile, presence: true,uniqueness: {message: "mobile number already used"}
