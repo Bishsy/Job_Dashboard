1086e785599cb413e762098754b09e29fe6ff9f3
Luffyv2.0 <devenderyadav1098@gmail.com>
2021-11-24 13:45:03 +0530

    applied for error is resolved

--- app/controllers/applied_for_controller.rb
+++ app/controllers/applied_for_controller.rb
@@ -6,17 +6,19 @@ class AppliedForController < ApplicationController
     end
     def new
         @applied_for = AppliedFor.new
+        @job=Job.find(params[:job_id])
     end
     def create
         @jseeker = Jseeker.find(session[:jseeker_id])
-        #@user = User.find_by(params[:user_id])
-        @job=Job.find_by(id: params[:id])
+        
+        @job=Job.find(params[:id])
+        
         
         
         if AppliedFor.where(jseeker_id:@jseeker.id, job_id:@job).present?
             redirect_to jseeker_path(@jseeker), notice: @job.id
         else
-            @a = AppliedFor.new(job_id: @job, jseeker_id:@jseeker.id ,status: "1")
+            @a = AppliedFor.new(job_id: @job.id, jseeker_id:@jseeker.id ,status: "1")
             if @a.save
                 redirect_to jseeker_path(@jseeker), notice: "Applied successfully"
             else
